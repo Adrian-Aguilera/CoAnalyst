a372ac0a4ebbd1e9b303de8654fbcd23d31f73cb
Adrian-Aguilera <110703102+Adrian-Aguilera@users.noreply.github.com>
2024-06-08 18:31:14 -0600

    add: conexion db

--- DB/Conexion.php
+++ DB/Conexion.php
@@ -1,1 +1,14 @@
 <?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$database="tds11b";
+
+$conn= new mysqli($servername,$username,$password,$database);
+
+if($conn->connect_error){
+    die('Conexion Fallida'.$conn->connect_error);
+}else{
+    echo "<script>alert('Conexion Exitosa')</script>";
+}
