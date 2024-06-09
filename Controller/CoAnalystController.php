4cb56b4b185a36621a9b4846859671be7915dc6e
Adrian-Aguilera <110703102+Adrian-Aguilera@users.noreply.github.com>
2024-06-08 23:08:14 -0600

    conexion db

--- Controller/CoAnalystController.php
+++ Controller/CoAnalystController.php
@@ -1,6 +1,7 @@
 <?php
 require "Rest_Controller.php";
 require "Parser_Controller.php";
+require "../DB/Conexion.php";
 
 class get_data{
     public function data_input(){
@@ -14,6 +15,8 @@ class get_data{
 $rest_model = new Rest_Controller();
 $parser_model = new Parser_Controller();
 $data_input = new get_data();
+$conexion_modelo = new Coanalystdb();
+$conexion_data = $conexion_modelo->getConnection();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     list ($codigo, $lenguaje) = $data_input->data_input();
