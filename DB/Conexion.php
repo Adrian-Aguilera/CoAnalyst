3b7066132995b349a8d29931f6ca658b652f5e96
Adrian-Aguilera <110703102+Adrian-Aguilera@users.noreply.github.com>
2024-06-08 18:43:51 -0600

    refactor: cambios codigo conexion db

--- DB/Conexion.php
+++ DB/Conexion.php
@@ -1,14 +1,19 @@
 <?php
-
-$servername="localhost";
-$username="root";
-$password="";
-$database="tds11b";
-
-$conn= new mysqli($servername,$username,$password,$database);
-
-if($conn->connect_error){
-    die('Conexion Fallida'.$conn->connect_error);
-}else{
-    echo "<script>alert('Conexion Exitosa')</script>";
+require_once '../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../CoAnalyst/');
+$dotenv->load();
+class Coanalystdb{
+    public function Conection(){
+        $Server_name = $_ENV["Server_name"];
+        $User_Name = $_ENV["User_Name"];
+        $Password = $_ENV["Password"];
+        $DB_name = $_ENV["DB_name"];
+        
+        $conexion_db= new mysqli($Server_name,$User_Name,$Password,$DB_name);
+        if($conexion_db->connect_error){
+            die('Conexion Fallida'.$conexion_db->connect_error);
+        }else{
+            echo "<script>alert('Conexion Exitosa')</script>";
+        }
+    }
 }
