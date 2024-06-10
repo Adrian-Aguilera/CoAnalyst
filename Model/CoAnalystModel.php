49b9e7fd7dc6c76cd62d202b33368ac77c72724b
Adrian-Aguilera <110703102+Adrian-Aguilera@users.noreply.github.com>
2024-06-09 22:22:46 -0600

    funcion para insertar

--- Model/CoAnalystModel.php
+++ Model/CoAnalystModel.php
@@ -7,6 +7,7 @@ class AlldataModel {
     private $conexion_modelo;
     private $conexion_data;
     private $all_resultados;
+    private $Insert_datos;
 
     public function __construct()
     {
@@ -17,6 +18,7 @@ class AlldataModel {
         $this->conexion_data = $this->conexion_modelo->getConnection();
         /*Querys cargar */
         $this->all_resultados = $_ENV["ALL_DATA"];
+        $this->Insert_datos = $_ENV["INSERT_DATA"];
 
     }
     public function AllDataRegister(){
@@ -31,6 +33,30 @@ class AlldataModel {
             return "Error en la conexión a la base de datos.";
         }
         return $resultados;
+    }
+    public function Insert_Datos($consulta_id, $tiempo_ejecucion, $complejidad, $memoria_usada,$tiempo, $estado){
+        $query_insert = $this->Insert_datos."('$consulta_id','$tiempo_ejecucion','$complejidad','$memoria_usada','$tiempo','$estado')";
+        $conexion_privada = $this->conexion_data;
+        if ($conexion_privada){
+            //$resultado = $conexion_privada->query($query_insert);
+            $instancia = $this->conexion_data->prepare($query_insert);
+            if ($instancia === false) {
+                die('Error al preparar la consulta: ' . $this->conexion_data->error);
+            }
+            $instancia->bind_param('sissss', $consulta_id, $tiempo_ejecucion, $complejidad, $memoria_usada, $tiempo, $estado);
+            $instancia->execute();
+            // Comprobar si la inserción fue exitosa
+            if ($instancia->affected_rows > 0) {
+                echo "Datos insertados correctamente.";
+            } else {
+                echo "No se pudo insertar los datos.";
+            }
+
+            // Cerrar la sentencia
+            $instancia->close();
+        }else{
+            echo "error conexion db";
+        }
     }
 }
 /*
