b5828bb12181d427606ad4c778c14bc76d8a51a0
ALEX\oscar <oscarel.gtr@gmail.com>
2024-06-13 00:44:48 -0600

    Reporte

--- Controller/Reportes.php
+++ Controller/Reportes.php
@@ -1,48 +1,41 @@
 <?php
-
 require_once '../vendor/autoload.php';
 require_once '../DB/Conexion.php';
-$pdf = new FPDF();
+use Fpdf\Fpdf;
+$pdf = new Fpdf();
+
 
 $pdf->AddPage();
 
-if($coneccion->connect_error){
+if($conn->connect_error){
     die("Conexion fallida: ".$conn->connect_error);
 }else{
-    $sql = "SELECT Username, password FROM usuarios";
+    $sql = "SELECT `Nombre`, `Tiempo de Ejecucion`, `Calidad`, `Complejidad` FROM `usuarios` ";
     $result =$conn->query($sql);
     $pdf->SetFont('Arial', 'B', 12);
-    $pdf->image('code.png', 4, 4, 40, 0);
-    $pdf->Cell(0, 10, 'Tabla de Usuarios', 0, 1, 'C');
+    $pdf->image('code.jpg', 4, 4, 40, 0);
+    $pdf->SetFont('Arial', 'B', 16);
+    $pdf->Cell(0, 40, 'Reporte de Analisis', 0, 30, 'C');
+    //Encabezados de la tabla
+    $pdf->SetFont('Arial', 'B', 12);
+    $pdf->Cell(40, 10, 'Nombre', 1);
+    $pdf->Cell(50, 10, 'Tiempo de Ejecucion', 1);
+    $pdf->Cell(40, 10, 'Calidad', 1);
+    $pdf->Cell(40, 10, 'Complejidad', 1);
     $pdf->Ln();
-    $pdf->SetFont('Arial', 'B', 10);
-    $anchoPagina = $pdf->GetPageWidth();
-    $anchoColumna = 50;
-    $posicionX = ($anchoPagina - $anchoColumna * 5)/5;
-    $pdf->SetX($posicionX);
-    $pdf->Cell($anchoColumna, 10, 'Nombre', 1, 0, 'C');
-    $pdf->Cell($anchoColumna, 10, 'Tiempo de Ejecucion', 1, 1, 'C');
-    $pdf->Cell($anchoColumna, 10, 'Calidad', 1, 1, 'C');
-    $pdf->Cell($anchoColumna, 10, 'Complejidad', 1, 1, 'C');
-    $pdf->SetFont('Arial', '', 10);
-
 }
 
-if ($result->num_rows >0){
-    while ($row = $result->fetch_assoc()){
-        $posicionX = ($anchoPagina -$anchoColumna * 5)/5;
-        $pdf->SetX($posicionX);
-        // Nombre de los campos en la db
-        $pdf->Cell($anchoColumna, 10, $row["Nombre"], 1, 0, 'C');
-        $pdf->Cell($anchoColumna, 10, $row["Tiempo de Ejecucion"], 1, 1, 'C');
-        $pdf->Cell($anchoColumna, 10, $row["Calidad"], 1, 1, 'C');
-        $pdf->Cell($anchoColumna, 10, $row["Complejidad"], 1, 1, 'C');
+$pdf->SetFont('Arial', '', 12);
+if ($result->num_rows > 0) {     
+    while($row = $result->fetch_assoc()) { 
+                $pdf->Cell(40, 10, $row["Nombre"], 1); 
+                $pdf->Cell(50, 10, $row["Tiempo de Ejecucion"], 1); 
+                $pdf->Cell(40, 10, $row["Calidad"], 1);
+                $pdf->Cell(40, 10, $row["Complejidad"], 1); 
+                $pdf->Ln(); 
+            }
+} else {    
+       $pdf->Cell(0, 10, 'No se encontraron resultados', 1, 1, 'C'); 
     }
-
-}else{
-    // Error por si no se encuentran los datos!
-    $pdf->Cell($anchoColumna *2, 10, 'No se encontraron usuarios');
-
-}
-ob_clean();
+$conn ->close();
 $pdf->Output();
